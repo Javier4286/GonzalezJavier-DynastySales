94e7d15dd1c88ec8c56e1036e3ace80421a5f43e
Javier4286 <130845254+Javier4286@users.noreply.github.com>
2023-06-09 16:31:38 -0300

    Delete Introducción a la programación directory

--- Introducción a la programación/Clase_7/functions/find.js
+++
@@ -1,8 +0,0 @@
-const {list} = require ("./list");
-
-function find(param){
-    const titulos = list();
-    return titulos.includes(param);
-}
-
-module.exports = { find };
--- Introducción a la programación/Clase_8/crud/list.js
+++
@@ -1,6 +0,0 @@
-const { read } = require("./read");
-function list() {
-  const data = read();
-  return data.map((tarea) => tarea.titulo);
-}
-module.exports = { list };
